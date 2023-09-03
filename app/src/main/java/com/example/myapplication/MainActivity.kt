c3b2119c2ac7a4073885557021a0c69a2a4b64c5
aDeus <aleksstroy22@gmail.com>
2023-09-03 12:49:35 +0300

    optim

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -4,25 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.itemsIndexed
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -49,35 +35,6 @@ class MainActivity : ComponentActivity() {
 
     }
 
-}
-
-
-@Composable
-private fun CircleBtn(){
-    val counter = remember {
-        mutableStateOf(0)
-    }
-    val color = remember {
-        mutableStateOf(Color.Blue)
-    }
-    Box(
-        modifier = Modifier
-            .size(100.dp)
-            .background(color.value, shape = CircleShape)
-
-            .clickable {
-                when (++counter.value) {
-                    10 -> color.value = Color.Red
-                }
-
-            },
-        contentAlignment = Alignment.Center
-    ){
-        Text(
-            text = counter.value.toString(),
-            style = TextStyle(Color.White, fontSize = 20.sp)
-        )
-    }
 }
 
 
